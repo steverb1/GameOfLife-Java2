92d2cc8c254a71b3f74be027cb1aec442f11d5c6
steve.bement <steve.bement@accenture.com>
2024-06-26 08:02:43 -0600

    first 3 tests

---
+++ GameOfLife/src/main/java/Cell.java
@@ -0,0 +1,32 @@
+import java.util.Objects;
+
+public class Cell {
+    int x;
+    int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cell cell = (Cell) o;
+        return x == cell.x && y == cell.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
